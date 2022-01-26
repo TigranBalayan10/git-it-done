cc152ac8f6b35139af3b3a6de3d377a0221dc1ae
TigranBalayan10 <tigranbalayan10@gmail.com>
2022-01-25 19:50:57 -0800

    add extra massege

--- assets/js/single.js
+++ assets/js/single.js
@@ -8,6 +8,9 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -15,7 +18,7 @@ var getRepoIssues = function (repo) {
     })
 };
 
-getRepoIssues("tigranbalayan10/run-buddy");
+getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -51,3 +54,17 @@ var displayIssues = function (issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
